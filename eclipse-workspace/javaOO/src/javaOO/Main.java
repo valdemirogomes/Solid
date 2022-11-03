f464b4c7d7937a6b7d3e5fbf0ca46abcb3f5cfc7
Valdemiro Gomes <valdemiro.gomes.rodrigues@gmail.com>
2022-11-03 15:46:35 -0300

    Classes e objetos

---
+++ eclipse-workspace/javaOO/src/javaOO/Main.java
@@ -0,0 +1,14 @@
+package javaOO;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Cliente c1 = new Cliente("Maria","José","056.490.092-89","03-11-2022");
+		c1.mostrarDados();
+		
+		Cliente c2 = new Cliente("Maria","Izabel","555.590.082-69","03-10-2022");
+		c2.mostrarDados();
+
+	}
+
+}
